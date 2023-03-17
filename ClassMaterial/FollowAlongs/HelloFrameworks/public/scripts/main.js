9aa3f7364058a5ca9548c097e1f3f0560791b99a
rhit-laneks <laneks@rose-hulman.edu>
2023-03-17 16:46:42 -0400

    data attributes

--- ClassMaterial/FollowAlongs/HelloFrameworks/public/scripts/main.js
+++ ClassMaterial/FollowAlongs/HelloFrameworks/public/scripts/main.js
@@ -3,34 +3,45 @@
  * Provides the JavaScript interactions for all pages.
  *
  * @author 
- * PUT_YOUR_NAME_HERE
+ * Kaylee Lane
  */
 
 /** namespace. */
 var rhit = rhit || {};
 
-/** globals */
-rhit.variableName = "";
+rhit.counter = 0;
 
-/** function and class syntax examples */
-rhit.functionName = function () {
-	/** function body */
-};
-
-rhit.ClassName = class {
-	constructor() {
-
-	}
-
-	methodName() {
-
-	}
-}
-
-/* Main */
-/** function and class syntax examples */
 rhit.main = function () {
 	console.log("Ready");
+	const buttons = document.querySelectorAll("#counterButtons button");
+	// for (let i = 0; i < buttons.length; i++) {
+	// 	const button = buttons[i];
+
+	// 	button.onclick = (event) => {
+	// 		console.log(`You pressed`, button);
+	// 	};
+	// }
+
+
+	//in iterates over the keys, of iterates over the values
+	for (const button of buttons) {
+		button.onclick = (event) => {
+			const dataAmount = button.dataset.amount;
+			const dataIsMultiplication = button.dataset.isMultiplication;
+			console.log(`Amount: ${dataAmount} isMult: ${dataIsMultiplication}`);
+		};
+
+	}
+
+	// buttons.forEach((button) => {
+	// 	button.onclick = (event) => {
+	// 		console.log(`You pressed`, button);
+	// 	};
+	// });
+
 };
 
+rhit.updateCounter = function () {
+
+};
 rhit.main();
