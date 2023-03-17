5e9fe57bf15abd56ac900ec383dfd80c7dfce9f3
rhit-laneks <laneks@rose-hulman.edu>
2023-03-17 17:55:06 -0400

    convert JS to use jQuery

--- ClassMaterial/FollowAlongs/HelloFrameworks/public/scripts/main.js
+++ ClassMaterial/FollowAlongs/HelloFrameworks/public/scripts/main.js
@@ -13,7 +13,9 @@ rhit.counter = 0;
 
 rhit.main = function () {
 	console.log("Ready");
-	const buttons = document.querySelectorAll("#counterButtons button");
+	// const buttons = document.querySelectorAll("#counterButtons button");
+
+
 	// for (let i = 0; i < buttons.length; i++) {
 	// 	const button = buttons[i];
 
@@ -24,30 +26,41 @@ rhit.main = function () {
 
 
 	//in iterates over the keys, of iterates over the values
-	for (const button of buttons) {
-		button.onclick = (event) => {
-			const dataAmount = parseInt(button.dataset.amount);
-			const dataIsMultiplication = button.dataset.isMultiplication == "true";
-			// console.log(`Amount: ${dataAmount} isMult: ${dataIsMultiplication}`);
-			// console.log(`Amount: ${typeof(dataAmount)} isMult: ${typeof(dataIsMultiplication)}`);
 
-			// if(dataIsMultiplication) {
-			// 	console.log("This IS Truthy. Use multiplication.");
-			// } else {
-			// 	console.log("Not multiplication.")
-			// }
+	// for (const button of buttons) {
+	// 	button.onclick = (event) => {
+	// 		const dataAmount = parseInt(button.dataset.amount);
+	// 		const dataIsMultiplication = button.dataset.isMultiplication == "true";
+	// 		// console.log(`Amount: ${dataAmount} isMult: ${dataIsMultiplication}`);
+	// 		// console.log(`Amount: ${typeof(dataAmount)} isMult: ${typeof(dataIsMultiplication)}`);
 
-			rhit.updateCounter(dataAmount, dataIsMultiplication);
+	// 		// if(dataIsMultiplication) {
+	// 		// 	console.log("This IS Truthy. Use multiplication.");
+	// 		// } else {
+	// 		// 	console.log("Not multiplication.")
+	// 		// }
 
-		};
+	// 		rhit.updateCounter(dataAmount, dataIsMultiplication);
 
-	}
+	// 	};
+
+	// }
 
 	// buttons.forEach((button) => {
 	// 	button.onclick = (event) => {
 	// 		console.log(`You pressed`, button);
 	// 	};
 	// });
+
+	$("#counterButtons button").click((event) => {
+		// console.log("Button", event.target);
+		const dataAmount = $(event.target).data("amount");
+		const dataIsMultiplication = !!$(event.target).data("isMultiplication");
+		rhit.updateCounter(dataAmount, dataIsMultiplication);
+		// console.log(`Amount: ${dataAmount} isMult: ${dataIsMultiplication}`);
+		// console.log(`Amount: ${typeof(dataAmount)} isMult: ${typeof(dataIsMultiplication)}`);
+
+	});
 
 };
 
@@ -57,7 +70,7 @@ rhit.updateCounter = function (amount, isMultiplication) {
 	} else {
 		rhit.counter = rhit.counter + amount;
 	}
-	document.querySelector("#counterText").innerHTML = `Count = ${rhit.counter}`;
-
+	// document.querySelector("#counterText").innerHTML = `Count = ${rhit.counter}`;
+	$("#counterText").html(`Count = ${rhit.counter}`);
 };
 rhit.main();
