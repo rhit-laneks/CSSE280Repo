b05fb740e1fd23f34b094be5bc6ec35d6ca61ee5
rhit-laneks <laneks@rose-hulman.edu>
2023-03-24 10:18:58 -0400

    create a movie quote

--- ClassMaterial/FollowAlongs/MovieQuotesNoAuth/public/scripts/main.js
+++ ClassMaterial/FollowAlongs/MovieQuotesNoAuth/public/scripts/main.js
@@ -5,11 +5,19 @@ rhit.FB_COLLECTION_MOVIEQUOTES = "MovieQuotes";
 rhit.FB_KEY_QUOTE = "quote";
 rhit.FB_KEY_MOVIE = "movie";
 rhit.FB_KEY_LAST_TOUCHED = "lastTouched";
-rhit.FbMovieQuoteManager = null;
+rhit.fbMovieQuotesManager = null;
 
 rhit.ListPageController = class {
 	constructor() {
-		console.log("created ListPageController");
+		// document.querySelector("#submitAddQuote").onclick = (event) => {
+		// };
+
+		document.querySelector("#submitAddQuote").addEventListener("click", (event) => {
+			const quote = document.querySelector("#inputQuote").value;
+			const movie = document.querySelector("#inputMovie").value;
+			rhit.fbMovieQuotesManager.add(quote, movie);
+
+		});
 	}
 	updateList() {}
    }
@@ -29,7 +37,23 @@ rhit.ListPageController = class {
 	  this._documentSnapshots = [];
 	  this._ref = firebase.firestore().collection(rhit.FB_COLLECTION_MOVIEQUOTES);
 	}
-	add(quote, movie) {    }
+	add(quote, movie) {  
+		console.log(`add quote ${quote}`);
+		console.log(`add movie ${movie}`);
+
+		// Add a new document with a generated id.
+		this._ref.add({
+			[rhit.FB_KEY_QUOTE]:quote,
+			[rhit.FB_KEY_MOVIE]: movie,
+			[rhit.FB_KEY_LAST_TOUCHED]: firebase.firestore.Timestamp.now(),
+		})
+		.then(function (docRef) {
+			console.log("Document written with ID: ", docRef.id);
+		})
+		.catch(function (error) {
+			console.error("Error adding document: ", error);
+		});
+	  }
 	beginListening(changeListener) {    }
 	stopListening() {    }
 	// update(id, quote, movie) {    }
@@ -46,7 +70,7 @@ rhit.main = function () {
 
 	if(document.querySelector("#listPage")) {
 		console.log("You are on the list page.");
-		rhit.FbMovieQuoteManager = new rhit.FbMovieQuotesManager();
+		rhit.fbMovieQuotesManager = new rhit.FbMovieQuotesManager();
 		new rhit.ListPageController();
 
 	}
