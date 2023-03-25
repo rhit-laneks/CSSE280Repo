e72c1c50cbe62b9cc898679ea76ec844a1e02bb3
rhit-laneks <laneks@rose-hulman.edu>
2023-03-24 20:49:49 -0400

    detail page scaffolding

--- ClassMaterial/FollowAlongs/MovieQuotesNoAuth/public/scripts/main.js
+++ ClassMaterial/FollowAlongs/MovieQuotesNoAuth/public/scripts/main.js
@@ -6,6 +6,7 @@ rhit.FB_KEY_QUOTE = "quote";
 rhit.FB_KEY_MOVIE = "movie";
 rhit.FB_KEY_LAST_TOUCHED = "lastTouched";
 rhit.fbMovieQuotesManager = null;
+rhit.fbSingleQuoteManager = null;
 
 // From: https://stackoverflow.com/questions/494143/creating-a-new-dom-element-from-an-html-string-using-built-in-dom-methods-or-pro/35385518#35385518
 function htmlToElement(html) {
@@ -98,7 +99,6 @@ rhit.ListPageController = class {
    
    rhit.FbMovieQuotesManager = class {
 	constructor() {
-	  console.log("created FbMovieQuotesManager");
 	  this._documentSnapshots = [];
 	  this._ref = firebase.firestore().collection(rhit.FB_COLLECTION_MOVIEQUOTES);
 	  this._unsubscribe = null;
@@ -150,7 +150,31 @@ rhit.ListPageController = class {
 		return mq;
 	 }
    }
+ 
+rhit.DetailPageController = class {
+	constructor() {
+	}
+	updateView() {  
+	}
+}
+
+rhit.FbSingleQuoteManager = class {
+	constructor(movieQuoteId) {
+	  this._documentSnapshot = {};
+	  this._unsubscribe = null;
+	  this._ref = firebase.firestore().collection(rhit.FB_COLLECTION_MOVIEQUOTES).doc(movieQuoteId);
+	}
+	beginListening(changeListener) {
+	}
+	stopListening() {
+	  this._unsubscribe();
+	}
+	update(quote, movie) {}
+	delete() {}
+   }
    
+   
+
 rhit.storage = rhit.storage || {};
 rhit.storage.MOVIEQUOTE_ID_KEY = "movieQuoteId";
 rhit.storage.getMovieQuoteId = function() {
@@ -180,10 +204,12 @@ rhit.main = function () {
 
 	if(document.querySelector("#detailPage")) {
 		console.log("You are on the detail page.");
-
 		const movieQuoteId = rhit.storage.getMovieQuoteId();
-		console.log(`Detail page for ${movieQuoteId}`);
-
+		if(!movieQuoteId) {
+			window.location.href = "/";
+		}
+		rhit.fbSingleQuoteManager = new rhit.FbSingleQuoteManager(movieQuoteId);
+		new rhit.DetailPageController();
 	}
 
 
