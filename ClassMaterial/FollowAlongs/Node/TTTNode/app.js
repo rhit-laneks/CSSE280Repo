121424ecbebdbd9ab96b734a733bd847e1e01dc5
rhit-laneks <laneks@rose-hulman.edu>
2023-04-24 10:45:31 -0400

    Simple Hello World HTTP trigger

--- ClassMaterial/FollowAlongs/Node/TTTNode/app.js
+++ ClassMaterial/FollowAlongs/Node/TTTNode/app.js
@@ -2,4 +2,28 @@ const express = require("express");
 
 var app = express();
 app.use(express.static("public"));
+
+app.get("/api/getmove/:board", (request, response) => {
+    const boardString = request.params.board;
+
+    const openLocations = getOpenLocations(boardString);
+
+    const moveSelected = openLocations[Math.floor(Math.random() * openLocations.length)];
+
+    response.send({"move": moveSelected});
+});
+
+function getOpenLocations(boardString) {
+    const openLocations = [];
+    for (var i = 0; i < boardString.length; i++) {
+      if (boardString.charAt(i) == '-') {
+        openLocations.push(i)
+      }
+    }
+    return openLocations;
+   }
+   
+   
+
+
 app.listen(3000);
